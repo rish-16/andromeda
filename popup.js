2aad654aeabc5209eb3f85b9ab2072ed19034f43
rish-16 <mail.rishabh.anand@gmail.com>
2022-01-01 23:53:52 +0800

    Remove popup.js and update popup HTML

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('latexify')
-    checkPageButton.addEventListener('click', function() {
-        console.log("latexified")
-        window.alert("latexified")
-    }, false)
-})
